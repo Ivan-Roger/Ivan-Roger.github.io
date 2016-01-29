22e7e302d67283a76ef200f910dcedbd8c4105b0
Ivan-Roger <ivan.bob.emile@gmail.com>
2016-01-29 18:16:16 +0100

    Petite mise-à-jour (en cours)

---
+++ js/dynamic-content.js
@@ -0,0 +1,15 @@
+function changeSubtitle() {
+  var subtitlesList = [
+    "Et bien plus ...",
+    "Ou sinon, contactez moi.",
+    "Les infos sont ici.",
+    "Quelques informations ..."
+  ]
+  var subtitle = subtitlesList[randomInt()%subtitlesList.length];
+  $("h2.project-tagline").text(subtitle);
+}
+
+$(function(){
+  $("h2.project-tagline").click(changeSubtitle);
+  changeSubtitle();
+})
